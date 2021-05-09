88137cb4a8b729eadb4e087219e0d8a2c6bacde3
Ling-Hong Hung <lhhunghimself@gmail.com>
2021-05-09 16:07:59 -0700

    Update guppySetup.sh

--- nanopore_guppy/widgets/nanopore_guppy/guppySetup/Dockerfiles/guppySetup.sh
+++ nanopore_guppy/widgets/nanopore_guppy/guppySetup/Dockerfiles/guppySetup.sh
@@ -4,10 +4,10 @@ build(){
     echo  "Will overwrite existing image"
     echo "docker build -t biodepot/guppy:gpu -f Dockerfile-$1 --build-arg gpu_url=$gpu_url ." && docker build -t biodepot/guppy:gpu -f Dockerfile-gpu --build-arg gpu_url=$gpu_url . && echo "build successfully" && return 0
   elif [ docker image inspect biodepot/guppy:gpu >/dev/null 2>&1 ]; then 
+	echo "image exists - will not build unless overwrite flag is set" && return 0
+  else
 	echo "no existing image - will build"
 	docker build -t biodepot/guppy:gpu -f Dockerfile-gpu --build-arg gpu_url=$gpu_url . && echo "build successfully" && return 0
-  else
-	echo "image exists - will not build unless overwrite flag is set" && return 0
   fi
   return 1
 }
